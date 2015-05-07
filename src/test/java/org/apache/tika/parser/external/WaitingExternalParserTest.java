f20327dcbc3406a70e2790321230b93c03e7289f
Ahmed Owian <ahmedowian@yahoo.com>
2015-05-07 16:51:53 -0400

    TIKA-634: Command Line Parser for Metadata Extraction    - Demonstrated ExternalParser concurrency bug

--- src/test/java/org/apache/tika/parser/external/WaitingExternalParserTest.java
+++ src/test/java/org/apache/tika/parser/external/WaitingExternalParserTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.tika.parser.external;
 
 import static org.junit.Assert.*;
@@ -17,6 +33,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+/**
+ * Unit-tests ExternalParser, demonstrating the need to wait for the external parser before returning from {parse}.
+ *
+ * @author Ahmed Owian
+ */
 public class WaitingExternalParserTest
 {
     private String[] command = {"cat", 
@@ -29,8 +50,11 @@ public class WaitingExternalParserTest
     {
         extractionPatterns = new HashMap<Pattern, String>();
         extractionPatterns.put(
-                Pattern.compile("Duration: (\\d+:\\d+:\\d+\\.?\\d?\\d?), "),
-                PBCore.INSTANTIATION_DURATION.getName());
+                        Pattern.compile("Duration: (\\d+:\\d+:\\d+\\.?\\d?\\d?), "),
+                        PBCore.INSTANTIATION_DURATION.getName());
+        extractionPatterns.put(
+                        Pattern.compile("(a+)"),
+                        PBCore.INSTANTIATION_DATA_RATE.getName());
     }
     
     @Test
@@ -64,8 +88,10 @@ public class WaitingExternalParserTest
         String[] names = extractedMetadata.names();
         String output = handler.toString();
         assertFalse(output, output.isEmpty());
-        assertEquals(1, names.length);
+        assertEquals(2, names.length);
         assertEquals(2, extractedMetadata.getValues(PBCore.INSTANTIATION_DURATION).length);
+        String[] values = extractedMetadata.getValues(PBCore.INSTANTIATION_DATA_RATE);
+        assertEquals(311, values.length);
     }
 
 }
